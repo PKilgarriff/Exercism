17bec780c7d44b437c2600d5228895715066b475
Paul <pkilgarriff@gmail.com>
2022-02-03 14:07:27 +0000

    Switch to using compact()

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,13 +1,8 @@
-=begin
-Write your code for the 'Flatten Array' exercise in this file. Make the tests in
-`flatten_array_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/flatten-array` directory.
-=end
-
 class FlattenArray
   def self.flatten(input)
-    return input.flatten.select { |item| item != nil }
+    # Use inbuilt flatten method to get 1D array, then select only values that aren't nil
+    # return input.flatten.select { |item| item != nil }
+    # Use inbuilt flatten method to get 1D array, then use compact method (removes empty values?)
+    return input.flatten.compact
   end
 end
