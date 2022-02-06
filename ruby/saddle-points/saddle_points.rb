f4447726cd4639bdfa44699a2330dd30520b6dc8
Paul <pkilgarriff@gmail.com>
2022-02-06 21:35:40 +0000

    Add initialize from Matrix exercise

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -1,7 +1,31 @@
-=begin
-Write your code for the 'Saddle Points' exercise in this file. Make the tests in
-`saddle_points_test.rb` pass.
+# Create a Matrix class
+# Create initialize fucntion to turn input string into matrix of integers
+# Cast integers to strings
+# Include way of accessing rows
+# Include way of accessing columns
+#### I have already completed a matrix exercise and can reuse that code ####
 
-To get started with TDD, see the `README.md` file in your
-`ruby/saddle-points` directory.
-=end
+# Work out minimum value in each column
+# Work out maximum value in each column
+# See if those values match?
+
+class Matrix
+	attr_reader :rows, :columns
+
+	def initialize(string)
+		@rows = []
+		# splitting the input string into an array of strings representing the rows
+		array = string.split("\n")
+		# going through each row string and making an array of actual integers
+		array.each { |row|
+			# splitting on the whitespace by default
+			numbers = row.split
+			@rows.push( numbers.map { |number| number.to_i } )
+		}
+		# Turns out there's a method for this exact situation, that works with a 2D array with equal numbers of rows and columns
+		@columns = @rows.transpose
+	end
+
+  def saddle_points
+  end
+end
