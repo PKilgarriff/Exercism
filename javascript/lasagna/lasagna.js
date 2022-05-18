5a1ec971e4177cb23b94336bd92837762fadc3a1
Paul <pkilgarriff@gmail.com>
2022-05-18 16:51:20 +0100

    Add logic for totalTimeInMinutes
    - tests don't seem to line up with description
    - total working time could mean total time 'already worked'

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -70,5 +70,6 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  let prepTime = preparationTimeInMinutes(numberOfLayers);
+  return prepTime + actualMinutesInOven;
 }
