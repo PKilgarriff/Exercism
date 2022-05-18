fa298b4edf20b355c0061ecac4053caedfb445bd
Paul <pkilgarriff@gmail.com>
2022-05-18 17:58:03 +0100

    Add logic for functions:
    - frontDoorResponse
    - frontDoorPassword
    - backDoorResponse

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -1,23 +1,4 @@
 // @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-//
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./door-policy.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Good luck with that door policy!
 
 /**
  * Respond with the correct character, given the line of the
@@ -27,7 +8,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line.charAt(0);
 }
 
 /**
@@ -38,7 +19,9 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  return word
+  .toLowerCase()
+  .replace(/\b\w/g, character => character.toUpperCase());
 }
 
 /**
@@ -49,7 +32,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  let trimmedLine = line.trim();
+  return trimmedLine.charAt(trimmedLine.length-1);
 }
 
 /**
