d3b0cee1a702fb178efd3a0c2ed3b77dbfef4636
Paul <pkilgarriff@gmail.com>
2022-06-04 12:10:08 +0100

    Add adjective_to_verb() logic

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -70,5 +70,6 @@ def adjective_to_verb(sentence, index):
 
     For example, ("It got dark as the sun set", 2) becomes "darken".
     """
-
-    pass
+    words = sentence.split(" ")
+    word = words[index].strip('.')
+    return word + "en"
