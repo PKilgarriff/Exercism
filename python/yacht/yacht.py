4ba452e4107588f119b15f2f39237654fc0a0715
Paul <pkilgarriff@gmail.com>
2022-06-08 11:11:57 +0100

    Add little_straight() and big_straight()

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -1,25 +1,29 @@
+def sum_occurrences(dice, value):
+    return sum(filter(lambda number: number == value, dice))
+
+
 def ones(dice):
-    return sum(filter(lambda number: number == 1, dice))
+    return sum_occurrences(dice, 1)
 
 
 def twos(dice):
-    return sum(filter(lambda number: number == 2, dice))
+    return sum_occurrences(dice, 2)
 
 
 def threes(dice):
-    return sum(filter(lambda number: number == 3, dice))
+    return sum_occurrences(dice, 3)
 
 
 def fours(dice):
-    return sum(filter(lambda number: number == 4, dice))
+    return sum_occurrences(dice, 4)
 
 
 def fives(dice):
-    return sum(filter(lambda number: number == 5, dice))
+    return sum_occurrences(dice, 5)
 
 
 def sixes(dice):
-    return sum(filter(lambda number: number == 6, dice))
+    return sum_occurrences(dice, 6)
 
 
 def calculate_total(dice):
@@ -33,6 +37,22 @@ def is_yacht(dice):
     return 0
 
 
+def little_straight(dice):
+    expected = [1, 2, 3, 4, 5]
+    dice.sort()
+    if dice == expected:
+        return 30
+    return 0
+
+
+def big_straight(dice):
+    expected = [2, 3, 4, 5, 6]
+    dice.sort()
+    if dice == expected:
+        return 30
+    return 0
+
+
 YACHT = is_yacht
 ONES = ones
 TWOS = twos
@@ -42,8 +62,8 @@ FIVES = fives
 SIXES = sixes
 FULL_HOUSE = None
 FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
+LITTLE_STRAIGHT = little_straight
+BIG_STRAIGHT = big_straight
 CHOICE = calculate_total
 
 
