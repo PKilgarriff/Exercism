882d028510563dd5eb29a40501d8aaded8c1cf95
Paul <pkilgarriff@gmail.com>
2022-06-20 14:45:01 +0100

    Remove accidental import

--- python/little-sisters-essay/string_methods.py
+++ python/little-sisters-essay/string_methods.py
@@ -1,9 +1,6 @@
 """Functions to help edit essay homework using string manipulation."""
 
 
-from hashlib import new
-
-
 def capitalize_title(title):
     """Convert the first letter of each word in the title to uppercase if needed.
 
