f275d1fd6481d8c691d3ad516913e296dc6c4d5d
Paul <pkilgarriff@gmail.com>
2022-06-20 15:10:46 +0100

    Implement square() and total()

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    else:
+        return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return sum([square(number) for number in range(1, 65)])
